cc5865cb7dc7503d2ce99eba7dbdca28849d9ba9
rkilchmn <roger@kilchenmann.net>
2025-01-13 17:13:59 +1100

    changes to make openai API work

--- start.ps1
+++ start.ps1
@@ -1,7 +1,7 @@
 cd C:\Users\I047787\Documents\project\whisper-writer
 .venv\Scripts\activate
 # Start the whisper server in WSL
-# Start-Process wsl -ArgumentList "--distribution Ubuntu-22.04 bash -c '/home/roger/projects/whisper.cpp/start-server.sh'" -WindowStyle Hidden
+Start-Process wsl -ArgumentList "--distribution Ubuntu-22.04 bash -c '/home/roger/projects/whisper.cpp/start-server.sh'" -WindowStyle Hidden
 
 # start remote-faster-whisper container
 # Start-Process wsl -ArgumentList "--distribution Ubuntu-22.04 bash -c '/home/roger/projects/remote-faster-whisper/docker-run.sh'" -WindowStyle Hidden
@@ -11,5 +11,5 @@ wsl.exe --distribution Ubuntu-22.04 --exec dbus-launch true
 
 # start python program
 # use local whisper API compatible with openai
-$env:OPENAI_API_BASE = "http://127.0.0.1:8080/inference"
+# not required anymore $env:OPENAI_API_BASE = "http://127.0.0.1:8080/inference"
 Start-Process python -ArgumentList "run.py" -WindowStyle Hidden
